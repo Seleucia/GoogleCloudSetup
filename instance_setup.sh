226d99739c7e2aab488926741c9661f15554f600
Seleucia <Seleucia@users.noreply.github.com>
2017-10-10 14:13:08 +0200

    Update instance_setup.sh

--- instance_setup.sh
+++ instance_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo apt-get install zip
-sudo apt-get install unzip
+sudo apt-get install zip unzip
 
 
 sudo apt-get update
@@ -17,28 +16,37 @@ sudo apt-get -y install cuda
 sudo modprobe nvidia
 nvidia-smi
 
+
+#remove cuda from the /use/local/cuda*
+rm -R  /use/local/cuda*
+
+wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
+mv cuda_8.0.61_375.26_linux-run cuda_8.0.61_375.26_linux.run
+sudo sh cuda_8.0.61_375.26_linux.run
+
+
 # CUDNN
-wget "https://www.dropbox.com/s/6bicqaxhom0n7yw/cudnn-8.0-linux-x64-v6.0.tgz?dl=0" -O "cudnn.tgz"
+wget "https://www.dropbox.com/s/1vxk9q579fobx6a/code.7z?dl=0" -O "cudnn.tgz"
 tar -zxf cudnn.tgz
 cd cuda
 sudo cp lib64/* /usr/local/cuda/lib64/
 sudo cp include/* /usr/local/cuda/include/
 
 echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64/" >>  ~/.bashrc
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
-
-# install Anaconda for current user
-mkdir downloads
-cd downloads
-wget "https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh" -O "Anaconda2-4.2.0-Linux-x86_64.sh"
-bash "Anaconda2-4.2.0-Linux-x86_64.sh" -b
-
-echo "export PATH=\"$HOME/anaconda2/bin:\$PATH\"" >> ~/.bashrc
-export PATH="$HOME/anaconda2/bin:$PATH"
-pip install tensorflow-gpu
 source ~/.bashrc
-cd ..
-cd ..
+	
+
+# install tensorflow
+
+sudo apt-get install libcupti-dev
+sudo apt-get install python-pip python-dev 
+pip install --upgrade pip
+
+sudo pip install tensorflow-gpu h5py 
+
+source ~/.bashrc
+python -c 'import tensorflow as tf; print tf.__version__'
+
 mkdir PycharmProjects
 cd PycharmProjects
 mkdir training
